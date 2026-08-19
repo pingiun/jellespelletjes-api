1e5156ff57a4d9fdb84d2e48b605077e29b23bc4
Jelle Besseling <jelle@pingiun.com>
2026-08-19 08:36:45 +0200

    Revert "Waitlist endpoint for the Goud paid account"
    This reverts commit 34a5c829b9627d0092680a08d825f7da833deb0f.

--- migrations/0007_goud_waitlist.sql
+++
@@ -1,5 +0,0 @@
--- Waitlist signups for the Jellespelletjes Goud paid account.
-CREATE TABLE goud_waitlist (
-  email TEXT PRIMARY KEY,
-  created_at TEXT NOT NULL DEFAULT (datetime('now'))
-);
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,4 @@
 pub mod auth;
-pub mod goud;
 pub mod profile;
 pub mod puzzle;
 pub mod results;
